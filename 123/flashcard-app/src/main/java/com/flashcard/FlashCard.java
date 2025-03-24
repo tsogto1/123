2b0629ba2bcbbc017d59b9a1fea9a72bc0ffda0c
tsogto1 <162122928+tsogto1@users.noreply.github.com>
2025-03-24 11:18:00 +0800

    Update FlashCard.java

--- 123/flashcard-app/src/main/java/com/flashcard/FlashCard.java
+++ 123/flashcard-app/src/main/java/com/flashcard/FlashCard.java
@@ -1,1 +1,25 @@
+package com.flashcard;
+
+public class FlashCard {
+    private String question;
+    private String answer;
+
+    public FlashCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Q: " + question + "\nA: " + answer;
+    }
+}
 
